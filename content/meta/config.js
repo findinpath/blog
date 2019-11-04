a471408dd3f8dbdccab36fa486681004efa747c3
findinpath <findinpath@gmail.com>
2019-11-05 00:07:39 +0100

    Initial commit. Adaptation of https://github.com/baobabKoodaa/blog

---
+++ content/meta/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteTitle: "find in path", // <title>
+  shortSiteTitle: "findinpath", // <title> ending for posts and pages
+  siteDescription: "Programming findings",
+  siteUrl: "https://www.findinpath.com",
+  // pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+
+  /* author */
+  authorName: "findinpath",
+  authorTwitterAccount: "",
+  authorGithub: "https://github.com/findinpath",
+
+  /* header text */
+  headerTitle: "find in path",
+  headerSubTitle: "",
+
+  /* manifest.json */
+  manifestName: "findinpath",
+  manifestShortName: "findinpath", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+};
